83bf506c38635063246d9f968ef1369ca7ef96c1
Nikoali Makarov <makarov.nikolai20@gmail.com>
2024-07-30 15:25:42 +0300

    some bugs were fixed: copy for src dir and relative to cwd pathes

--- src/model.py
+++ src/model.py
@@ -9,6 +9,8 @@ from torch import nn
 from torch.nn import functional as F
 
 
+CWD: str = os.getcwd()
+
 class WolfClassifier(nn.Module):
     def __init__(
         self,
@@ -17,8 +19,6 @@ class WolfClassifier(nn.Module):
 
         self.feature_extractor: nn.Module = torchaudio.pipelines.HUBERT_BASE.get_model()
 
-        self._cwd: str = os.getcwd()
-
         hidden_size: int = 0
         if hasattr(self.feature_extractor, 'encoder'):
             hidden_size = self.feature_extractor.encoder.transformer.layers[0].attention.k_proj.out_features
@@ -26,10 +26,10 @@ class WolfClassifier(nn.Module):
             hidden_size = self.feature_extractor.model.encoder.transformer.layers[0].attention.k_proj.out_features
 
         self.linear: nn.Linear = nn.Linear(hidden_size, 2)
-        if not Path(self._cwd).joinpath('./data/saved_weights.pth').exists():
+        if not Path(CWD).joinpath('./data/saved_weights.pth').exists():
             self.load_weights()
 
-        self.load_state_dict(torch.load(str(Path(self._cwd).joinpath('./data/saved_weights.pth')), map_location='cpu'))
+        self.load_state_dict(torch.load(str(Path(CWD).joinpath('./data/saved_weights.pth')), map_location='cpu'))
 
     def load_weights(self):
         base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'
@@ -46,7 +46,7 @@ class WolfClassifier(nn.Module):
             download_url,
             timeout=60,
         )
-        with open(str(Path(self._cwd).joinpath('./data/saved_weights.pth')), 'wb') as f:
+        with open(str(Path(CWD).joinpath('./data/saved_weights.pth')), 'wb') as f:
             f.write(download_response.content)
 
     @torch.inference_mode()
