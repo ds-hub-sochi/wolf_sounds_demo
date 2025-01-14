3e3d82e2dbb2a3077b09faabc4b679c8e87b3008
Nikolai Makarov <makarov.nikolai20@gmail.com>
2025-01-14 21:15:16 +0300

    streamlit app is about to be updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ from __future__ import annotations
 
 import json
 import os
-import pathlib
 import shutil
 from math import ceil
+from pathlib import Path
 from time import gmtime, strftime
 
 import streamlit as st
@@ -14,18 +14,32 @@ from stqdm import stqdm
 
 from src import constants
 from src import model
+from src import feature_extractor
 
+root_dir: Path = Path(__file__).parent
 
-CWD: str = os.getcwd()
-pathlib.Path(CWD).joinpath('data/processed').mkdir(
+(root_dir / 'data' / 'processed').mkdir(
     parents=True,
     exist_ok=True,
 )
 
-pathlib.Path(CWD).joinpath('saved_weights').mkdir(
+(root_dir / 'saved_weights').mkdir(
     parents=True,
     exist_ok=True,
 )
+
+(root_dir / 'data' /' processed' / 'audio_with_detected_wolf').mkdir(
+    parents=True,
+    exist_ok=True,
+)
+
+markup: dict[str, list[tuple[str, str]]] = {}
+with open(
+    root_dir / 'data' / 'processed' / 'timings.json',
+    'w',
+    encoding='utf-8',
+) as timings_file:
+    json.dump(markup, timings_file)
 
 if 'MODEL_SAMPLE_RATE' not in st.session_state:
     st.session_state.MODEL_SAMPLE_RATE = constants.MODEL_SAMPLE_RATE
@@ -39,39 +53,36 @@ if 'BATCH_SIZE' not in st.session_state:
 if 'CHUNKS_WITH_WOLF_RATE' not in st.session_state:
     st.session_state.CHUNKS_WITH_WOLF_RATE = constants.CHUNKS_WITH_WOLF_RATE
 
+if 'CONFIDENCE_THRESHOLD' not in st.session_state:
+    st.session_state.CONFIDENCE_THRESHOLD = 0.5
+
 if 'DEVICE' not in st.session_state:
     st.session_state.DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-if 'model' not in st.session_state:
-    st.session_state.model = model.WolfClassifier().to(st.session_state.DEVICE)
+if 'feature_extractor' not in st.session_state:
+    st.session_state.feature_extractor = feature_extractor.ASTConverter(st.session_state.MODEL_SAMPLE_RATE)
+
+if 'animal_classifier' not in st.session_state:
+    st.session_state.animal_classifier = model.ASTBasedClassifier(
+        "https://disk.yandex.ru/d/1Jz2-F7fArielA",
+        "animal_vs_no_animal",
+    ).to(st.session_state.DEVICE)
+
+if 'wolf_classifier' not in st.session_state:
+    st.session_state.wolf_classifier = model.ASTBasedClassifier(
+        "https://disk.yandex.ru/d/S4m1-1AV-O10pQ",
+        "wolf_vs_other_animal",
+    ).to(st.session_state.DEVICE)
+
 
 uploaded_files = st.file_uploader(
-    'Выберете wav-файлы. Суммарный вес не должен превышать 6 GB',
+    'Choose wav-файлы. The total weight must not exceed 6 GB',
     accept_multiple_files=True,
     type='wav',
 )
 
-markup: dict[str, list[tuple[str, str]]] = {}
-with open(
-    pathlib.Path(CWD).joinpath('data/processed/timings.json'),
-    'w',
-    encoding='utf-8',
-) as f:
-    json.dump(markup, f)
-
-pathlib.Path(CWD).joinpath('data/processed/audio_with_detected_wolf').mkdir(
-    parents=True,
-    exist_ok=True,
-)
-
-CONFIDENCE_THRESHOLD = st.number_input(
-    'Минимальное значение для уверенности модели в наличии воя',
-    value=0.5,
-    help='от 0.0 до 1.0; чем ниже значение - тем большее количество записей без воя будет помечено, как с наличием воя.'
-)
-
-if CONFIDENCE_THRESHOLD is not None:
-    if st.button(label='Начать разметку'):
+if True:
+    if st.button(label='Classify'):
         for uploaded_file in stqdm(uploaded_files):
             bytes_data = uploaded_file.read()
             file_name: str = uploaded_file.name
@@ -79,6 +90,7 @@ if CONFIDENCE_THRESHOLD is not None:
             waveform, sample_rate = torchaudio.load(
                 bytes_data,
                 backend='ffmpeg',
+                # backend='soundfile',
             )
 
             if sample_rate != st.session_state.MODEL_SAMPLE_RATE:
@@ -130,14 +142,19 @@ if CONFIDENCE_THRESHOLD is not None:
                     )
                     current_tensors[-1] = padding(current_tensors[-1])
 
+                features: list[torch.Tensor] = [st.session_state.feature_extractor.convert(wav) for wav in current_tensors]
+
                 batch: torch.Tensor = torch.stack(
-                    current_tensors,
+                    features,
                     dim=0,
                 )
-                batch = batch.squeeze(1).to(st.session_state.DEVICE)
 
-                current_file_markup.extend(list(st.session_state.model.get_wolf_probability(batch).cpu().numpy()))
+                print(batch.shape)
+                # batch = batch.squeeze(1).to(st.session_state.DEVICE)
 
+                # current_file_markup.extend(list(st.session_state.model.get_wolf_probability(batch).cpu().numpy()))
+
+        """
             markup[file_name] = []
 
             running_duration: float = 0.0
@@ -226,3 +243,4 @@ if CONFIDENCE_THRESHOLD is not None:
         all_files = os.listdir(pathlib.Path(CWD).joinpath('data/processed/audio_with_detected_wolf'))
         for file in all_files:
             os.remove(pathlib.Path(CWD).joinpath(f'data/processed/audio_with_detected_wolf/{file}'))
+        """
