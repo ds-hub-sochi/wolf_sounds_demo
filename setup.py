57ed8118dcf9e77d51d0ade7338bb5ec5a87f38b
Nikolai Makarov <makarov.nikolai20@gmail.com>
2025-01-15 17:11:29 +0300

    project files updated

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='wolf_sounds_demo',
+    name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Local wolf sound demo',
+    description='Wolf howl detection demo',
     author='Nikolai Makarov @ ds-hub-sochi',
     license='',
 )
